97f5f4a24201d522928a5092d3bffe16f514304c
AlexeyKryuk <43128289+AlexeyKryuk@users.noreply.github.com>
2022-10-25 17:10:52 +0300

    Correct Pointer

--- Assets/_Scripts/Spells/ProjectorPointer.cs
+++ Assets/_Scripts/Spells/ProjectorPointer.cs
@@ -7,14 +7,36 @@ public class ProjectorPointer : MonoBehaviour
     [SerializeField] private GameObject _pointerPrefab;
 
     private Transform _pointer;
+    private CardsMover _cardsMover;
+
+    private bool _isCardInHand;
 
     private void Awake()
     {
-        _pointer = Instantiate(_pointerPrefab).transform;    
+        _pointer = Instantiate(_pointerPrefab).transform;
+        _cardsMover = FindObjectOfType<CardsMover>();
+    }
+
+    private void OnEnable()
+    {
+        _cardsMover.CardTaken += OnCardTaken;
+        _cardsMover.CardDrop += OnCardDrop;
+    }
+
+    private void OnDisable()
+    {
+        _cardsMover.CardTaken -= OnCardTaken;
+        _cardsMover.CardDrop -= OnCardDrop;
     }
 
     private void Update()
     {
+        if (_isCardInHand == false)
+        {
+            _pointer.gameObject.SetActive(false);
+            return;
+        }
+
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit[] hits = Physics.RaycastAll(ray, 100f);
 
@@ -26,8 +48,18 @@ public class ProjectorPointer : MonoBehaviour
             if (hit.collider.TryGetComponent(out ICardApplicable applicable))
             {
                 _pointer.gameObject.SetActive(true);
-                _pointer.position = hit.point;
+                _pointer.position = hit.point + Vector3.up * 0.1f;
             }
         }
+    }
+
+    private void OnCardTaken()
+    {
+        _isCardInHand = true;
+    }
+
+    private void OnCardDrop()
+    {
+        _isCardInHand = false;
     }
 }
