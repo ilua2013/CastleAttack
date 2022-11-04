2592eb7b49bc8c800fc3183e37c1c4702bd9eb00
AlexeyKryuk <43128289+AlexeyKryuk@users.noreply.github.com>
2022-11-04 12:28:24 +0300

    Update HealthBar.cs

--- Assets/_Scripts/UI/HealthBarUnits/HealthBar.cs
+++ Assets/_Scripts/UI/HealthBarUnits/HealthBar.cs
@@ -6,37 +6,33 @@ using UnityEngine.UI;
 
 public class HealthBar : MonoBehaviour
 {
+    private const float LerpTime = 1f;
+    private const float LerpError = 0.01f;
+
     [SerializeField] private Fighter _fighter;
     [SerializeField] private Slider _bar;
 
     private void OnEnable()
     {
-        _fighter.Damaged += OnTakeDamage;
-        _fighter.Healed += OnRecovery;
+        _fighter.Damaged += OnHealthChanged;
+        _fighter.Healed += OnHealthChanged;
     }
 
     private void OnDisable()
     {
-        _fighter.Damaged -= OnTakeDamage;
-        _fighter.Healed -= OnRecovery;
+        _fighter.Damaged -= OnHealthChanged;
+        _fighter.Healed -= OnHealthChanged;
     }
 
-    private void OnTakeDamage(int damage)
+    private void OnHealthChanged(int amount)
     {
         float remain = (float)_fighter.Health / _fighter.MaxHealth;
-        StartCoroutine(LerpValue(remain, 1f));
-    }
-
-    private void OnRecovery(int amount)
-    {
-        float remain = (float)_fighter.Health / _fighter.MaxHealth;
-
-        StartCoroutine(LerpValue(remain, 1f));
+        StartCoroutine(LerpValue(remain, LerpTime));
     }
 
     private IEnumerator LerpValue(float to, float time)
     {
-        while (MathF.Abs(_bar.value - to) > 0.01f)
+        while (MathF.Abs(_bar.value - to) > LerpError)
         {
             _bar.value = Mathf.MoveTowards(_bar.value, to, time * Time.deltaTime);
             yield return new WaitForEndOfFrame();
