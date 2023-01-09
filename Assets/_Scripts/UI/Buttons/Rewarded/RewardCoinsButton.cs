4f7d87d2d4bca158a1019eb495fc2141d4e92118
AlexeyKryuk <43128289+AlexeyKryuk@users.noreply.github.com>
2023-01-09 16:48:56 +0300

    Reward button

--- Assets/_Scripts/UI/Buttons/Rewarded/RewardCoinsButton.cs
+++ Assets/_Scripts/UI/Buttons/Rewarded/RewardCoinsButton.cs
@@ -1,26 +1,39 @@
 using Agava.YandexMetrica;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 [RequireComponent(typeof(Button))]
 public class RewardCoinsButton : MonoBehaviour
 {
-    [SerializeField] private int _award;
     [SerializeField] private CoinsWallet _wallet;
+    [SerializeField] private LevelRewardData _levelRewardData;
+    [SerializeField] private TMP_Text _text;
 
+    private int _award;
     private Button _button;
 
     private void OnValidate()
     {
         if (_wallet == null)
             _wallet = FindObjectOfType<CoinsWallet>();
+
+        if (_levelRewardData == null)
+            _levelRewardData = Resources.Load("Configs/LevelReward") as LevelRewardData;
     }
 
     private void Awake()
     {
         _button = GetComponent<Button>();
+
+        if (Saves.HasKey(SaveController.Params.Level))
+            _award = _levelRewardData.GetAward(Saves.GetInt(SaveController.Params.Level) - 2).Coins;
+        else
+            _award = 100;
+
+        _text.text = _award.ToString();
     }
 
     private void OnEnable()
