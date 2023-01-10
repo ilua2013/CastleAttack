ac7454050765e07d7db326962037c5f9588280a4
AlexeyKryuk <43128289+AlexeyKryuk@users.noreply.github.com>
2023-01-10 15:00:32 +0300

    Commit

--- Assets/_Scripts/Units/Fighter.cs
+++ Assets/_Scripts/Units/Fighter.cs
@@ -95,6 +95,22 @@ public class Fighter
         if (_type == FighterType.Archer || _type == FighterType.Catapult)
         {
             Arrow arrow = _unit.SpawnArrow(_arrow, transform);
+            float delay = 0.15f;
+
+            arrow.FlyTo(fighter.transform.position, fighter, () =>
+            {
+                onEnd?.Invoke();
+            }, () =>
+            {
+                isFatal = fighter.TakeDamage(this);
+            }, delay);
+
+            Attacked?.Invoke();
+            Attacked_get?.Invoke(fighter.transform);
+        }
+        else if (_type == FighterType.Catapult)
+        {
+            Arrow arrow = _unit.SpawnArrow(_arrow, transform);
             Cell cell = _unit.Mover.CurrentCell?.Bot?.Bot;
             float delay = 0.15f;
 
